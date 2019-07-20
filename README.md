d1be8ed11b0e39882c2e70aa462fb9fee452e205
Noah Braam <nwbraam@gmail.com>
2019-07-20 08:27:26 -0400

    generate passwords, update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # go-pass
-Password generator command line tool in Go
+Password generator command line tool in Go.
+
+## Usage
+`gopass` generates a password of lenght 12.
+`gopass -l x` generates a password of length x.
+`gopass -n x` generates x passwords of length 12.
+`gopass -1 x -n y` generates y passwords of length x.
--- gopass.go
+++ gopass.go
@@ -3,17 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 var passLen, numPasses int
 
+const passChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-_=+"
+
 func init() {
+	rand.Seed(time.Now().UnixNano())
 	flag.IntVar(&passLen, "l", 12, "length of the password")
 	flag.IntVar(&numPasses, "n", 1, "number of passwords")
 	flag.Parse()
 }
 
 func main() {
-	fmt.Println("Hello!")
-	fmt.Println("Pass Len: ", passLen, " Num Passes: ", numPasses)
+	fmt.Println("Password Length: ", passLen, " Num Passwords: ", numPasses)
+	for i := 0; i < numPasses; i++ {
+		pass := randPass(passLen)
+		fmt.Println(pass)
+	}
+}
+
+func randPass(l int) string {
+	b := make([]byte, l)
+	for i := range b {
+		b[i] = passChars[rand.Intn(len(passChars))]
+	}
+	return string(b)
 }
